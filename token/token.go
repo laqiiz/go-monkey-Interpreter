238adb6c57c5d6fb4352f1aa42a0748b293f91d7
laqiiz <mano.junki@gamil.com>
2020-06-13 12:15:24 +0900

    p23 repl implement

--- token/token.go
+++ token/token.go
@@ -3,23 +3,32 @@ package token
 type Type string
 
 type Token struct {
-	Type Type
+	Type    Type
 	Literal string
 }
 
 const (
 	ILLEGAL = "ILLEGAL"
-	EOF = "EOF"
+	EOF     = "EOF"
 
 	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT = "INT"
+	INT   = "INT"
+
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	// 演算子
-	ASSIGN = "="
-	PLUS = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 
 	// delimiter
-	COMMA = ","
+	COMMA     = ","
 	SEMICOLON = ";"
 
 	LPAREN = "("
@@ -29,5 +38,27 @@ const (
 
 	// keyword
 	FUNCTION = "FUNCTION"
-	LET = "LET"
+	LET      = "LET"
+	TRUE     = "true"
+	FALSE    = "false"
+	IF       = "if"
+	ELSE     = "else"
+	RETURN   = "return"
 )
+
+var keywords = map[string]Type{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
